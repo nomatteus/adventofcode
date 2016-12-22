77833e7dd2b9f575d2105e54bda0419672fe6b36
nomatteus <matt@ruten.ca>
2016-12-22 05:51:31 -0600

    day 7 part 2

--- 2016/src/Day07.java
+++ 2016/src/Day07.java
@@ -1,5 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -8,18 +10,6 @@ import java.util.Scanner;
 public class Day07 {
 
     public boolean supportsTLS(String ip) {
-        /*
-        Ideas....
-        * Is this like a parser? Can we scan through character by character?
-        * And then consume character by character, saving 3 prev chars as lookbehind.
-        *    We are looking for the abba-type sequence.
-        * And use square brackets to determine the "mode". Once we see a [, we enter reject mode,
-        *    which means if we see an abba-sequence, we immediately reject (return false).
-        *    Note that we also throw away all previous characters when changing modes.
-        *    When we see a ], then we enter accept mode, and when we find an abba-sequence in this mode,
-        *       we set a boolean flag to true (but still need to check the entire string, since there may
-        *       be another abba in square brackets that will cause a rejection)
-         */
         // Store the 3 previous seen characters
         Character prev1 = null;
         Character prev2 = null;
@@ -57,6 +47,71 @@ public class Day07 {
         return abbaSequenceFound;
     }
 
+    /*
+     The difference here is that we need to find an 'aba' occurrence outside of square brackets,
+        as well as a matching 'bab' occurrence in square brackets.
+     The logic to find these sequences is the same as part 1, except we only need to store the previous 2
+        characters. But, in addition, we need to store the actual characters of all the matches, so we can
+        tell if there is a match for the corresponding bab.
+        i.e. we do a pass through the string to find all aba/bab strings in and out of square brackets,
+            and then do a matching step to find if there are any matches in the 2 sets.
+     Data Structure Notes:
+        - When an aba set is found outside of square brackets, store in array list as [a,b]
+        - when bab set is found inside square brackets, store in arrayList in reverse, as [a,b]
+        This way, when these 2 matching items are compared, they will be equal.
+        - Then, store the inside and outside square brackets in a Set-based data structure.
+        - Then, at the end, do an intersection (in java, that's retainAll()), and if there are
+            any elements in the intersection, then return true (since there is a match)
+     */
+    public boolean supportsSSL(String ip) {
+        // Store the 2 previous seen characters
+        Character prev1 = null;
+        Character prev2 = null;
+
+        // Init data structure to store matches.
+        HashSet<ArrayList<Character>> outsideSquareBrackets = new HashSet<>();
+        HashSet<ArrayList<Character>> insideSquareBrackets = new HashSet<>();
+
+        // Boolean to represent 2 modes: reject and accept (in square brackets or not)
+        // Start off not in square brackets.
+        boolean inSquareBrackets = false;
+        for (Character curChar : ip.toCharArray() ) {
+            // Note that we do not support nested square brackets, so this simplifies things.
+            if (curChar.charValue() == '[' || curChar.charValue() == ']') {
+                inSquareBrackets = !inSquareBrackets ;
+                // Null out previous chars
+                prev1 = null;
+                prev2 = null;
+            } else {
+                if (curChar.equals(prev2) && !curChar.equals(prev1)) {
+                    ArrayList<Character> match = new ArrayList<>();
+
+                    if (inSquareBrackets) {
+                        // Note that we add this in reverse order (so we will be able to compare
+                        // using equality)
+                        match.add(prev1);
+                        match.add(curChar);
+                        insideSquareBrackets.add(match);
+                    } else {
+                        match.add(curChar);
+                        match.add(prev1);
+                        outsideSquareBrackets.add(match);
+                    }
+                }
+
+                // Set previous chars for next iteration
+                prev2 = prev1;
+                prev1 = curChar;
+            }
+        }
+        // Check if there are any matches.
+        // Perform set intersection ("retainAll") to modify outsideSquareBrackets set to
+        // only contain the items that also are in insideSquareBrackets set
+        outsideSquareBrackets.retainAll(insideSquareBrackets);
+        // If there are any elements left then we have at least one match.
+        return outsideSquareBrackets.size() > 0;
+    }
+
     public static void main(String[] args) throws Exception {
         Day07 day7 = new Day07();
         String[] ips = new String(Files.readAllBytes(Paths.get("src/inputs/day07"))).split("\n");
@@ -66,9 +121,15 @@ public class Day07 {
                 countPart1++;
             }
         }
+        int countPart2 = 0;
+        for (String ip : ips) {
+            if (day7.supportsSSL(ip)) {
+                countPart2++;
+            }
+        }
         System.out.print("--> Part 1: ");
         System.out.println(countPart1);
-//        System.out.print("--> Part 2: ");
-//        System.out.println(countPart2);
+        System.out.print("--> Part 2: ");
+        System.out.println(countPart2);
     }
 }
--- 2016/test/Day07Test.java
+++ 2016/test/Day07Test.java
@@ -11,11 +11,16 @@ public class Day07Test {
     @Test
     public void test_Day07() {
         Day07 day7 = new Day07();
+        // part 1
         assertEquals(true, day7.supportsTLS("abba[mnop]qrst"));
         assertEquals(false, day7.supportsTLS("abcd[bddb]xyyx"));
         assertEquals(false, day7.supportsTLS("aaaa[qwer]tyui"));
         assertEquals(true, day7.supportsTLS("ioxxoj[asdfgh]zxcvbn"));
-
+        // part 2
+        assertEquals(true, day7.supportsSSL("aba[bab]xyz"));
+        assertEquals(false, day7.supportsSSL("xyx[xyx]xyx"));
+        assertEquals(true, day7.supportsSSL("aaa[kek]eke"));
+        assertEquals(true, day7.supportsSSL("zazbz[bzb]cdb"));
     }
 
 }
