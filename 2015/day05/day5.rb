df9f66d50bb8ef5d66ae40f37bff0d2d979a9af7
nomatteus <matt@ruten.ca>
2017-12-09 15:14:08 -0500

    day 5 part 2

--- 2015/day05/day5.rb
+++ 2015/day05/day5.rb
@@ -20,3 +20,35 @@ input_lines.each do |line|
 end
 
 puts "Part 1 nice count: #{nice_count}"
+
+## PART 2
+
+nice_count = 0
+input_lines.each do |line|
+
+  # Store start index of letter pairs
+  letter_pairs = {}
+
+  line_chars = line.chars
+  # repeat char with exactly one letter between
+  has_repeat_sandwich = false
+  line_chars.each_with_index do |char, i|
+    # check letter pairs condition
+    if i > 0
+      pair = "#{line_chars[i-1]}#{char}"
+      letter_pairs[pair] ||= []
+      # check for an overlapping pair
+      if !letter_pairs[pair].include?(i-1)
+        letter_pairs[pair] << i
+      end
+    end
+
+    # check for "repeat "sandwich"
+    has_repeat_sandwich = true if i > 1 && char == line_chars[i-2]
+  end
+
+  has_pair_of_letter_pairs = letter_pairs.values.collect(&:size).any? { |s| s > 1 }
+  nice_count += 1 if has_pair_of_letter_pairs && has_repeat_sandwich
+end
+
+puts "Part 2 nice count: #{nice_count}"
