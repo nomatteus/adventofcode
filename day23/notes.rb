519e2652d0b79ca5e175f01e65c6b731228dcb1d
nomatteus <matt@ruten.ca>
2017-12-30 18:48:31 -0600

    add some notes

--- day23/notes.rb
+++ day23/notes.rb
@@ -14,17 +14,25 @@ while 1 != 0 do
   f = 1
   d = 2      # d acts as a counter, incrementing by 1 on each loop iteration
 
-  do
+  # CURRENT INTUITION: This is an algorithm searching for 2-number integer factorizations of
+  #  numbers (i.e. values of b). If there is a 2-number factorization for a value of b, then
+  #  h is incremented by 1
+  do  # for d from 2 to b
     e = 2   # e acts as a counter, incrementing by 1 on each loop iteration
-    do
+    do  # for e from 2 to b
+      # First outer iteration b=106700, d=2, e=2,3,4... -> ... 2*2, 2*3, 2*4, etc... so will have 53350 iterations before this is true
+      # Second outer iteration, b = 106717, d=3, e=2,3,4 => ... 3*2, 3*3, 3*4, etc.. never true, since 106717 not divisible by 3
+      # third outer iteration, b = 106734, d=4,
+      # next outer iteration , b = 106751, d=5,
+      # next outer iteration , b = 106768, d=6,
       if (d * e) == b
         f = 0
       end
       e += 1
-    while e != b   # simplified from g !=0  -->  (e-b) != 0
+    while e != b      # simplified from g !=0  -->  (e-b) != 0
 
     d += 1
-  while d != b # Simplified from  g != 0 ->  d - b != 0
+  while d != b        # Simplified from  g != 0 ->  d - b != 0
 
   if f == 0
     h = h + 1
