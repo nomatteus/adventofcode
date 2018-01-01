a6b311bd61c2ebbb5a54243565c1df4d1d15f962
nomatteus <matt@ruten.ca>
2018-01-01 04:29:30 -0600

    Day 24 part 2
    And cleanup the code a bit

--- day24/day24.rb
+++ day24/day24.rb
@@ -1,21 +1,9 @@
-require 'pry'
 input = IO.read('./input').strip.split("\n")
-# input = [ #test
-#   '0/2',
-#   '2/2',
-#   '2/3',
-#   '3/4',
-#   '3/5',
-#   '0/1',
-#   '10/1',
-#   '9/10',
-# ]
 
 class Component
-  attr_accessor :component_id, :left_port, :right_port
+  attr_accessor :left_port, :right_port
 
-  def initialize(component_id, left_port, right_port)
-    @component_id = component_id
+  def initialize(left_port, right_port)
     @left_port = left_port
     @right_port = right_port
   end
@@ -33,57 +21,87 @@ class ElectromagneticMoat
   attr_accessor :components
 
   def initialize(input)
-    @components = {}
+    @components = []
     parse_input(input)
   end
 
   # Starting with the 0-port components, find the one with the max value
   # (Assumes all 0-ports have 0 as the left port, which is true for the given input)
   def find_max_bridge_strength
-    zero_components = @components.values.select(&:has_zero_pin_port?)
-    potential_bridges = zero_components.collect { |c| max_bridge([c], c.right_port, c.strength) }
-    potential_bridges.collect { |b| b[1] }.max
+    # Finds the best bridge strength for each of the starting 0-components
+    potential_bridges = zero_components.collect { |c| max_bridge_strength([c], c.right_port, c.strength) }
+    potential_bridges.collect { |b| b[:strength] }.max
   end
 
   # Recursive method to find the maximum bridge strength
-  def max_bridge(used_components, next_connector, strength=0)
+  def max_bridge_strength(used_components, next_connector, strength=0)
     next_options = find_unused_components(used_components, next_connector)
-    return [used_components, strength] if next_options.empty?
-    max = next_options.collect do |options|
-      next_component, next_next_connector = options
-      max_bridge(used_components + [next_component], next_next_connector, strength + next_component.strength)
-    end.max { |bridge1, bridge2| bridge1[1] <=> bridge2[1] }
-    max
+
+    # Base Case: We are at the end of a chain of components, so return the chain and the strength
+    return { used_components: used_components, strength: strength } if next_options.empty?
+
+    # Recursive case: Search all possible options from the current component, and return the best one
+    next_options.collect do |option|
+      max_bridge_strength(used_components + [option[:component]], option[:next_connector], strength + option[:component].strength)
+    end.max { |bridge1, bridge2| bridge1[:strength] <=> bridge2[:strength] }
   end
 
+  # Part 2: Find the max bridge length, breaking ties by choosing the max strength
+  def find_max_bridge_length
+    potential_bridges = zero_components.collect { |c| max_bridge_length([c], c.right_port) }
+    potential_bridges.max do |b1, b2|
+      # Strength is tiebreaker
+      b1.size == b2.size ? b1.sum(&:strength) <=> b2.sum(&:strength) : b1.size <=> b2.size
+    end.sum(&:strength)
+  end
+
+  # Recursive method to find the maximum bridge length
+  def max_bridge_length(used_components, next_connector)
+    next_options = find_unused_components(used_components, next_connector)
+
+    # Base case: No more matching components that could make the chain longer, so return the chain
+    return used_components if next_options.empty?
+
+    # Recursive Case:
+    next_options.collect do |option|
+      max_bridge_length(used_components + [option[:component]], option[:next_connector])
+    end.max do |b1, b2|
+      b1.size == b2.size ? b1.sum(&:strength) <=> b2.sum(&:strength) : b1.size <=> b2.size
+    end
+  end
+
+  # Finds all unused components that match the given connection port number.
+  # Will also return the unused port for each connection
   def find_unused_components(used_components, next_connector)
-    matches = @components.values.collect do |component|
+    @components.collect do |component|
+      # We need to return the next_connector, so on the next iteration we will know what to search for (i.e. which port is already used)
       if component.left_port == next_connector
-        # Format is the matching component, and next next connector
-        [component, component.right_port]
+        { component: component, next_connector: component.right_port }
       elsif component.right_port == next_connector
-        component
-        [component, component.left_port]
+        { component: component, next_connector: component.left_port }
       else
         nil
       end
-    end.compact
-    matches.reject { |match| used_components.include?(match[0]) }
+    end.compact.reject { |match| used_components.include?(match[:component]) }
   end
 
 private
 
+  def zero_components
+    @components.select(&:has_zero_pin_port?)
+  end
+
   def parse_input(input)
-    # Assign each component a unique ID
-    component_id = 1
     input.each do |component|
       left_port, right_port = component.split('/').map(&:to_i)
-      @components[component_id] = Component.new(component_id, left_port, right_port)
-      component_id += 1
+      @components << Component.new(left_port, right_port)
     end
   end
 end
 
-part1 = ElectromagneticMoat.new(input)
-puts "Part 1: #{part1.find_max_bridge_strength}"
+electromagnetic_moat = ElectromagneticMoat.new(input)
+puts "Part 1: #{electromagnetic_moat.find_max_bridge_strength}"
+
+part2 = ElectromagneticMoat.new(input)
+puts "Part 2: #{electromagnetic_moat.find_max_bridge_length}"
 
