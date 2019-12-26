07cd5921207aeb439ea19b656c2800e11cda025b
nomatteus <matt@ruten.ca>
2019-12-26 03:15:27 -0600

    Add solution for day 9 part 2

--- 2019/day09/day9.rb
+++ 2019/day09/day9.rb
@@ -3,10 +3,22 @@ require './intcode_day9'
 
 input = IO.read('input')
 program = Intcode::Helpers.parse_program(input)
-computer = Intcode::Computer.new(program: program, input: [1], debug: true)
 
-while !computer.terminated? do
-  puts computer.run
+puts "Part 1:"
+computer_testmode = Intcode::Computer.new(program: program, input: [1], debug: false)
+
+while !computer_testmode.terminated? do
+  puts computer_testmode.run
 end
 
 # Part 1: 3280416268
+
+
+puts "Part 2:"
+computer_sensormode = Intcode::Computer.new(program: program, input: [2], debug: false)
+
+while !computer_sensormode.terminated? do
+  puts computer_sensormode.run
+end
+
+# Part 2: 80210
