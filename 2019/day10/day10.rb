a2f79f866f5b62e763b25a7889e7bd916d88f516
nomatteus <matt@ruten.ca>
2019-12-26 21:02:46 -0600

    Slight refactor of day 10 part 1

--- 2019/day10/day10.rb
+++ 2019/day10/day10.rb
@@ -15,38 +15,59 @@ input = IO.read(input_file).strip.split.map { |row| row.split('').map { |val| tr
 # Use columns, as we want to access via x, y coords (where default for matrix is row, col)
 grid = Matrix.columns(input)
 
-asteroids = {}
+# Note that keyword_init requires Ruby 2.5+
+OtherAsteroidInfo = Struct.new(:asteroid, :angle, :distance, keyword_init: true)
 
-grid.each_with_index do |val, x, y|
-  if val == ASTEROID
-    asteroids[Vector[x, y]] = { vector: Vector[x, y] }
+class Asteroid
+  # other_asteroids is a list of OtherAsteroidInfo objects
+  attr_accessor :position, :other_asteroids
+
+  def initialize(x, y)
+    @position = Vector[x, y]
+  end
+
+  def num_asteroids_can_see
+    @num_asteroids_can_see ||= other_asteroids.map(&:angle).uniq.size
+  end
+
+  def ==(other)
+    self.position == other.position
   end
 end
 
-asteroids.each do |asteroid, asteroid_info|
+asteroids = []
+
+grid.each_with_index do |val, x, y|
+  if val == ASTEROID
+    asteroids << Asteroid.new(x, y)
+  end
+end
+
+asteroids.each do |asteroid|
   asteroid_transposed = Vector[0, 0]
 
-  angles = asteroids.each_with_object({}) do |(other_asteroid, other_asteroid_info), angles_result|
+  other_asteroids = asteroids.map do |other_asteroid|
     next if other_asteroid == asteroid
 
     # Transpose to have asteroid at 0,0
-    other_asteroid_transposed = other_asteroid - asteroid
+    other_asteroid_transposed = other_asteroid.position - asteroid.position
 
     x = other_asteroid_transposed[0]
     y = other_asteroid_transposed[1]
     angle = Math.atan2(y, x)
 
-    angles_result[other_asteroid] = angle
-  end
+    OtherAsteroidInfo.new(
+      asteroid: other_asteroid,
+      angle: angle,
+      distance: other_asteroid_transposed.magnitude
+    )
+  end.compact
 
-  asteroid_info[:angles] = angles
-  asteroid_info[:num_asteroids_can_see] = angles.values.uniq.size
+  asteroid.other_asteroids = other_asteroids
 end
 
 
-part1_asteroid = asteroids.max_by { |coords, info| info[:num_asteroids_can_see] }
-part1_coords = part1_asteroid[0]
-part1_num_can_see = part1_asteroid[1][:num_asteroids_can_see]
+part1_asteroid = asteroids.max_by(&:num_asteroids_can_see)
 
 # Solution: 326 (at location: Vector[22, 28])
-puts "Part 1: #{part1_num_can_see} (at location: #{part1_coords})"
+puts "Part 1: #{part1_asteroid.num_asteroids_can_see} (at location: #{part1_asteroid.position})"
