4f718205c3908d95aca84956bd870d95902cfab2
nomatteus <matt@ruten.ca>
2019-12-27 02:12:48 -0600

    refactor day 10 part 2

--- 2019/day11/day11.rb
+++ 2019/day11/day11.rb
@@ -6,69 +6,86 @@ require_relative '../day09/intcode_day9'
 
 input = IO.read('input')
 program = Intcode::Helpers.parse_program(input)
-robot = Intcode::Computer.new(program: program, debug: false)
 
-# Color values
-BLACK = 0
-WHITE = 1
+class PaintingRobot
+  attr_reader :painted_panels
 
-# Output codes
-OUTPUT_LEFT = 0
-OUTPUT_RIGHT = 1
+  # Color values
+  BLACK = 0
+  WHITE = 1
 
-# Direction vectors
-UP = Vector[0, 1]
-DOWN = Vector[0, -1]
-LEFT = Vector[-1, 0]
-RIGHT = Vector[1, 0]
+  # Output codes
+  OUTPUT_LEFT = 0
+  OUTPUT_RIGHT = 1
 
-# Store panel as a hash where key is a Vector and value is current color
-# All panels start as black, so set that as default.
-panel_grid = Hash.new(BLACK)
+  # Direction vectors
+  UP = Vector[0, 1]
+  DOWN = Vector[0, -1]
+  LEFT = Vector[-1, 0]
+  RIGHT = Vector[1, 0]
 
-current_location = Vector[0, 0]
-current_direction = UP
+  def initialize(program:, starting_color:)
+    @program = program
+    @robot = Intcode::Computer.new(program: program, debug: false)
+    @starting_color = starting_color
 
-# Track all panels that have been painted at least once
-painted_panels = Set.new
+    # Store panel as a hash where key is a Vector and value is current color
+    # All panels start as black, so set that as default.
+    @panel_grid = Hash.new(BLACK)
 
-while !robot.terminated? do
-  panel_color = panel_grid[current_location]
+    @current_location = Vector[0, 0]
+    @current_direction = UP
 
-  # Input: 0 if over black panel, 1 if over white panel
-  robot.add_input(panel_color)
+    # Set initial color
+    @panel_grid[@current_location] = starting_color
 
-  # Output 1: First, it will output a value indicating the color to paint the
-  # panel the robot is over: 0 means to paint the panel black, and 1 means to
-  # paint the panel white.
-  new_color = robot.run
-  panel_grid[current_location] = new_color
-
-  painted_panels << current_location
-
-  # Output 2: Second, it will output a value indicating the direction the robot
-  # should turn: 0 means it should turn left 90 degrees, and 1 means it should turn right 90 degrees.
-  direction = robot.run
-  if direction == OUTPUT_LEFT
-    # Turn left 90 degress
-    current_direction = case current_direction
-    when UP then LEFT
-    when LEFT then DOWN
-    when DOWN then RIGHT
-    when RIGHT then UP
-    end
-  else
-    # Turn right 90 degrees
-    current_direction = case current_direction
-    when UP then RIGHT
-    when RIGHT then DOWN
-    when DOWN then LEFT
-    when LEFT then UP
-    end
+    # Track all panels that have been painted at least once
+    @painted_panels = Set.new
   end
 
-  # Move forward one space
-  current_location += current_direction
+  def run!
+    while !@robot.terminated? do
+      panel_color = @panel_grid[@current_location]
+
+      # Input: 0 if over black panel, 1 if over white panel
+      @robot.add_input(panel_color)
+
+      # Output 1: First, it will output a value indicating the color to paint the
+      # panel the robot is over: 0 means to paint the panel black, and 1 means to
+      # paint the panel white.
+      new_color = @robot.run
+      @panel_grid[@current_location] = new_color
+
+      @painted_panels << @current_location
+
+      # Output 2: Second, it will output a value indicating the direction the robot
+      # should turn: 0 means it should turn left 90 degrees, and 1 means it should turn right 90 degrees.
+      direction = @robot.run
+      if direction == OUTPUT_LEFT
+        # Turn left 90 degress
+        @current_direction = case @current_direction
+        when UP then LEFT
+        when LEFT then DOWN
+        when DOWN then RIGHT
+        when RIGHT then UP
+        end
+      else
+        # Turn right 90 degrees
+        @current_direction = case @current_direction
+        when UP then RIGHT
+        when RIGHT then DOWN
+        when DOWN then LEFT
+        when LEFT then UP
+        end
+      end
+
+      # Move forward one space
+      @current_location += @current_direction
+    end
+  end
 end
 
-puts "Part 1: #{painted_panels.size} painted panels." # 2226
+part1_robot = PaintingRobot.new(program: program, starting_color: PaintingRobot::BLACK)
+part1_robot.run!
+
+puts "Part 1: #{part1_robot.painted_panels.size} painted panels." # 2226
