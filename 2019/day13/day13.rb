fcb3ff099a0bf73df14eddf4d56e7db9f431a27c
nomatteus <matt@ruten.ca>
2019-12-28 20:40:46 -0600

    Add day 13 display

--- 2019/day13/day13.rb
+++ 2019/day13/day13.rb
@@ -19,11 +19,24 @@ class Game
   }
   TILE_IDS = TILES.invert
 
+  # Characters used to display each tile
+  TILE_DISPLAY = {
+    empty: ' ',
+    wall: '█',
+    block: '▩',
+    horiz_paddle: '▂',
+    ball: '●',
+  }
+
+  CLEAR_TERMINAL = "\e[H\e[2J"
+
   def initialize(program:, debug: false)
     @computer = Intcode::Computer.new(program: program, input: [], debug: debug)
 
     # Hash of tile positions to their type
     @tiles = Hash.new(:empty)
+
+    @score = 0
   end
 
   def run
@@ -37,6 +50,31 @@ class Game
       @tiles[position] = TILES[tile_id]
     end
   end
+
+  # Display the current game board
+  def display
+    puts CLEAR_TERMINAL
+    minx = tiles.keys.map(&:x).min
+    maxx = tiles.keys.map(&:x).max
+    miny = tiles.keys.map(&:y).min
+    maxy = tiles.keys.map(&:y).max
+
+    miny.upto(maxy).each do |y|
+      minx.upto(maxx).each do |x|
+        pos = Position.new(x, y)
+        print TILE_DISPLAY[@tiles[pos]]
+      end
+      print "\n"
+    end
+
+    score_padded = "Score: #{@score}".center(41)
+    puts <<~STR
+      ███████████████████████████████████████████
+      ┌─────────────────────────────────────────┐
+      │#{score_padded}│
+      └─────────────────────────────────────────┘
+    STR
+  end
 end
 
 game = Game.new(program: program)
@@ -44,3 +82,5 @@ game.run
 
 part1 = game.tiles.values.count(:block)
 puts "Part 1: #{part1} block tiles" # 452
+
+game.display
