38d30759fd7610e07a6d3a99b092fe47c430404c
nomatteus <matt@ruten.ca>
2021-12-17 01:35:36 -0500

    add puzzle inputs

---
+++ 2021/day05/day5.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require 'matrix'
+
+#input = IO.read('./input').strip.split
+input = IO.read('./input_small').strip.split
+
+part1 = 
+
+puts "Part 1: #{part1}"
+
+
+# Part 2
+
+part2 = 
+
+puts "Part 2: #{part2}"
