c65a8a08658f59e38ad6782f9a8e4d62041e795b
nomatteus <matt@ruten.ca>
2022-12-22 05:05:36 -0600

    2022 day 13 part 1 - refactor using comparator

--- 2022/day13/day13.rb
+++ 2022/day13/day13.rb
@@ -7,44 +7,53 @@ input_file = "./input"
 # Parse input, and use `JSON.load` to convert to arrays
 input = IO.read(input_file).strip.split("\n\n").map { |group| group.split("\n").map { |item| JSON.load(item) } }
 
-def is_right_order?(left_array, right_array)
-  puts "-- is_right_order called with:\n#{left_array}\n#{right_array}"
-  left = 0
-  right = 0
+class Packet
+  include Comparable
+  attr_reader :value
 
-  while true
-    left_val = left_array[left]
-    right_val = right_array[right]
+  def initialize(value)
+    @value = value
+  end
 
-    if left_val.is_a?(Integer) && right_val.is_a?(Integer)
-      # Compare integers
-      return true if left_val < right_val
-      return false if left_val > right_val
-      # if they're the same value, we'll continue checking
-    elsif left_val.nil? && right_val.nil?
-      # arrays were the same size and result inconclusive, so keep checking...
-      return nil
-    elsif left_val.nil?
-      # left side ran out of items first, so this is in right order
-      return true
-    elsif right_val.nil?
-      # right side ran out of items first, so not in right order
-      return false
-    else
-      # Either both or one of the items is array, so compare as arrays
-      result = is_right_order?(Array(left_val), Array(right_val))
-      # If result is nil (inconclusive), then continue checking
-      return result unless result.nil?
+  def <=>(other_packet)
+    puts "-- <=> called with:\n#{value}\n#{other_packet.value}"
+    left = 0
+    right = 0
+
+    while true
+      left_val = value[left]
+      right_val = other_packet.value[right]
+
+      if left_val.is_a?(Integer) && right_val.is_a?(Integer)
+        # Compare integers
+        return 1 if left_val < right_val
+        return -1 if left_val > right_val
+        # if they're the same value, we'll continue checking
+      elsif left_val.nil? && right_val.nil?
+        # arrays were the same size and result inconclusive, so keep checking...
+        return 0
+      elsif left_val.nil?
+        # left side ran out of items first, so this is in right order
+        return 1
+      elsif right_val.nil?
+        # right side ran out of items first, so not in right order
+        return -1
+      else
+        # Either both or one of the items is array, so compare as arrays
+        result = Packet.new(Array(left_val)) <=> Packet.new(Array(right_val))
+
+        # If result is 0 (inconclusive), then continue checking
+        return result unless result.zero?
+      end
+
+      left += 1
+      right += 1
     end
-
-    left += 1
-    right += 1
   end
 end
 
-
 part1 = input.map.with_index do |(left, right), i| 
-  result = is_right_order?(left, right)
+  result = Packet.new(left) > Packet.new(right)
   puts "pair #{i + 1} is #{!result ? 'NOT ' : ''}in right order\n============================"
 
   # Sum indexes in right order for part 1
